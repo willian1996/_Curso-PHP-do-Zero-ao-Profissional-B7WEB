e74dfbeb61e8d5f084a34765e2dcc0ea3a2fcc3f
Willian Sales <williansalesgabriel@hotmail.com>
2019-05-16 03:52:37 -0300

    Aula 16/05

---
+++ Modulo-12_PHP_Avancado/Aula48-Projeto-Sistema-de-Reservas-4/bd_reservas.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: 16-Maio-2019 às 02:37
+-- Versão do servidor: 5.7.21
+-- PHP Version: 7.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bd_reservas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `carros`
+--
+
+DROP TABLE IF EXISTS `carros`;
+CREATE TABLE IF NOT EXISTS `carros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `carros`
+--
+
+INSERT INTO `carros` (`id`, `nome`) VALUES
+(1, 'Palio '),
+(2, 'Voyage '),
+(3, 'Corolla'),
+(4, 'Hilux '),
+(5, 'Camaro'),
+(6, 'Gol'),
+(7, 'Golf');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `reservas`
+--
+
+DROP TABLE IF EXISTS `reservas`;
+CREATE TABLE IF NOT EXISTS `reservas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_carro` int(11) DEFAULT NULL,
+  `data_inicio` date DEFAULT NULL,
+  `data_fim` date DEFAULT NULL,
+  `pessoa` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `reservas`
+--
+
+INSERT INTO `reservas` (`id`, `id_carro`, `data_inicio`, `data_fim`, `pessoa`) VALUES
+(1, 2, '2019-04-22', '2019-04-26', 'Willian '),
+(2, 4, '2019-04-07', '2019-04-08', 'Paulo'),
+(8, 2, '2019-04-08', '2019-04-11', 'Everton'),
+(7, 3, '2019-04-02', '2019-04-05', 'Otavia'),
+(6, 5, '2019-04-08', '2019-04-12', 'Bruna'),
+(9, 4, '2019-04-10', '2019-04-24', 'Cricrano '),
+(10, 7, '2019-04-27', '2019-05-05', 'Rayane');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
