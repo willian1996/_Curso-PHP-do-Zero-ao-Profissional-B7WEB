bb5943daf62046f1ec302e33e0d6360368ef59c1
Willian Sales <williansalesgabriel@hotmail.com>
2019-06-11 02:05:41 -0300

    Aula 11/06

---
+++ Modulo-16-PHP-Super-Avancado/1-CRUD-Operacoes-via-modal/adicionar_submit.php
@@ -0,0 +1,11 @@
+<?php 
+require_once "contato.class.php";
+$contato = new Contato();
+
+if(!empty($_POST["email"])){
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    
+    $contato->adicionar($email,$nome);
+    header("Location: index.php");
+}
